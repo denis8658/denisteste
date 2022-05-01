0e42fe0c29c2f6cb67bbe609c4b02c8c00195383
denis8658 <102334429+denis8658@users.noreply.github.com>
2022-04-30 21:13:48 -0300

    estudo-urls2

--- denisteste/urls.py
+++ denisteste/urls.py
@@ -13,17 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def emprestimobancario(request):
-    return HttpResponse('ola Denis')
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', emprestimobancario),
+    path('', include('Receitas.urls')),
 ]
