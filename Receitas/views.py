e6a80d36683ac2643f788c36bc4ffe02cefe05cd
denis8658 <102334429+denis8658@users.noreply.github.com>
2022-05-01 13:33:58 -0300

    aula-urls3

--- Receitas/views.py
+++ Receitas/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
 
 # Create your views here.
-
-
-def home(request):
+def home(Request):
     return HttpResponse('ola Denis 3')
